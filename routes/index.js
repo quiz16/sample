23faba0d55702fa81ad0834472f29e573abd6c73
quiz16 <kanaquiz16@gmail.com>
2013-10-24 00:49:12 +0800

    change require

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,15 @@
-var sql = require('./example');
+var sql = require('./sql_functions');
 
 exports.index = function(req, res){
   var data = {};
   data.title = "< >..T..raffix";
-  sql.load({'id' : '12'},function(err,result){
+  sql.browse('12','users','id',function(result){
 
   data.sample = result;
   console.log('result: ' + data.sample);
- 
+  res.render("index.ejs", data);
  });
- console.log(data.sample);
+ //console.log(data.sample);
  
-  res.render("index", data);
+ 
 };
